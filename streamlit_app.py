b712ba6e57ab0d47f893ce6a3a26d6e4480f0a54
Maybelle Auw <103557809+yxmauw@users.noreply.github.com>
2024-08-18 07:27:53 +0000

    first update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,46 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Zoonosis Scoring App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "The algorithm for the diagnosis " 
+    "and management of common zoonoses has " 
+    "been developed, incorporating the advice and "
+    "comments of GPs and laboratory and infectious "
+    "disease specialists."
 )
+st.subheader("Limitations of the algorithm")
+st.markdown("""
+* non-exhaustive
+* many medical conditions can present with signs
+    and symptoms that mimic zoonoses
+* Individual variability in the presentations 
+    of zoonotic illness
+* GPs urged to consult infectious disease specialists
+    and medical microbiologists for further guidance
+"""
+)
+
+st.subheader("Suspect zoonotic infection if the patient has:")
+st.multiselect("Clinical symptoms", 
+               ["fever >= 38°C",
+                "fever more than 1 occasion",
+                "headache", "chills", "myalgia", 
+                "fatigue", "dry cough", 
+                "shortness of breath", 
+                "general malaise"])
+st.multiselect("Risk factors", 
+               ["Non-household contact with farm "
+                "animals or wildlife", 
+                "Employment in agriculture, "
+                "Meat processing, dairy or "
+                "veterinary industries", 
+                "Non-work-related contact with "
+                "animals esp. cattle, sheep, pigs, "
+                "dogs and rodents", 
+                "Exposure to animal tissues or "
+                "animal products e.g. birth fluids",
+                "Involvement in feral pig hunting, "
+                "carcass processing, transporting "
+                "or inspection for export",
+                "Tick bites"])
+
