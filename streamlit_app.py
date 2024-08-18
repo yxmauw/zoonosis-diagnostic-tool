db231a9b13d9f70c045b262eb91082ef0734ef3f
Maybelle Auw <103557809+yxmauw@users.noreply.github.com>
2024-08-19 00:46:38 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -216,7 +216,7 @@ with tab2:
     def display_pdf(filepath):
         with open(filepath,"rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-            pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+            pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
 
     display_pdf("zoonoses.pdf")
