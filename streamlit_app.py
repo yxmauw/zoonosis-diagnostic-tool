a8cccd45bd96801be218e28d74a21246aa7f5c2c
Maybelle Auw <103557809+yxmauw@users.noreply.github.com>
2024-08-19 00:45:47 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -214,7 +214,7 @@ with tab1:
 
 with tab2: 
     def display_pdf(filepath):
-        with open(file_path,"rb") as f:
+        with open(filepath,"rb") as f:
             base64_pdf = base64.b64encode(f.read()).decode('utf-8')
             pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
             st.markdown(pdf_display, unsafe_allow_html=True)
