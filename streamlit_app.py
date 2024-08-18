e2e97ecb7c7f67d9c4cb0d0f5bd87eb28cc28ff1
Maybelle Auw <103557809+yxmauw@users.noreply.github.com>
2024-08-19 00:42:58 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -213,11 +213,11 @@ with tab1:
                 "Please consider other differential diagnoses.")
 
 with tab2: 
-    def show_pdf(file_path):
-        with open(file_path,"rb") as f:
-            base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-        pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-        st.markdown(pdf_display, unsafe_allow_html=True)
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
     
-    show_pdf("zoonoses.pdf")
+        pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    
+        st.markdown(pdf_display, unsafe_allow_html=True)
+
     st.markdown("[Reference]")
